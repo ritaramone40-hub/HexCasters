73d8d2bade35ca6152c3807f6a1be0daaad3e21b
ritaramone40-hub <ritaramone40@gmail.com>
2026-04-11 14:58:14 -0700

    Fix SSE: disable proxy buffering, reconnect on error

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.get('/room/:code/events', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
+  res.setHeader('X-Accel-Buffering', 'no');
   res.flushHeaders();
 
   if (!clients[code]) clients[code] = [];
